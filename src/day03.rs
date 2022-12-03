da988064b4ab54d253c78d29b60e676288f19101
David Maxwell <damaxwell@alaska.edu>
2022-12-03 08:43:16 -0900

    day 3 cleanup

--- src/day03.rs
+++ src/day03.rs
@@ -4,8 +4,6 @@ use std::fs::{File};
 use std::io::{BufReader,BufRead};
 use anyhow::{anyhow,Result,Context,Error};
 
-
-
 pub fn solve(args: &AppArgs) -> Result<Solution> {
 
   let input_path = args.input_file_path();
@@ -13,13 +11,14 @@ pub fn solve(args: &AppArgs) -> Result<Solution> {
                 .with_context(|| format!("Opening file: {}", &input_path) )? );
 
 
-  let mut spots:[u8;256] = [0;256];
-  let mut score:[u8;256] = [0;256];
-  let mut badges:[[u8;256];3] = [[0;256];3];
+  let mut bp_contents:[u8;256] = [0;256];
 
+  let mut backpacks:[ [u8;3]; 256] = [[0;3]; 256];
+
+  let mut letter_score:[u8;256] = [0;256];
   for k in 0..26 {
-    score[ (b'a' + k) as usize ] = (k+1);
-    score[ (b'A' + k) as usize ] = (k+27);
+    letter_score[ (b'a' + k) as usize ] = (k+1);
+    letter_score[ (b'A' + k) as usize ] = (k+27);
   }
 
   let mut star1:i64 = 0;
@@ -34,37 +33,37 @@ pub fn solve(args: &AppArgs) -> Result<Solution> {
     let n = line.len()/2;
     // Record which characters appear in the first half.
     for &b in &bytes[0..n] {
-      spots[b as usize ] = 1;
+      bp_contents[b as usize ] = 1;
     }
     // Determine which character in the second half showed up in the first half.
     for &b in &bytes[n..2*n] {
-      if spots[b as usize] == 1 {
-        star1 += score[b as usize] as i64;
+      if bp_contents[b as usize] == 1 {
+        star1 += letter_score[b as usize] as i64;
         break;
       }
     }
     // Clear the list of first half characters.
     for &b in &bytes[0..n] {
-      spots[b as usize] = 1;
+      bp_contents[b as usize] = 0;
     }
  
     // Star 2 computation
+    let bp_group_index = j%3;
 
-    // Record the list of objects in this backpack.
-    for b in bytes {
-      badges[(j%3)][*b as usize] = 1;
+    // Record the list of all objects in this backpack.
+    for &b in bytes {
+      backpacks[b as usize][bp_group_index] = 1;
     }
 
     // If we're at a multiple of 3, check which object appears in all three backpacks
     if (j%3) == 2 {
-      for k in 0..26 {
-        for c in [(b'a'+k) as usize , (b'A'+k) as usize] {
-          if (badges[0][c] == 1) && (badges[1][c] == 1) && (badges[2][c] == 1) {
-            star2 += score[c] as i64;
+      for base_char in [b'a', b'A'] {
+        for c in base_char..(base_char + 26) {
+          let c:usize = c.into();
+          if backpacks[c].iter().all(|s| *s == 1) {
+            star2 += letter_score[c] as i64;            
           }
-          badges[0][c] = 0;
-          badges[1][c] = 0;
-          badges[2][c] = 0;
+          backpacks[c].fill(0);
         }
       }
     }
